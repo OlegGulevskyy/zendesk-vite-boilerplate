b9db25286c1e284c7afff32f77d7f4ee7169a59b
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-27 19:20:18 +0200

    build sidebar into a dist folder

--- packages/sidebar/vite.config.ts
+++ packages/sidebar/vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+	
+  // TODO - need to remove ASSETS from built JS files
+  base: "./",
+	build: {
+		outDir: `${process.env.INIT_CWD}/dist/assets/sidebar`
+	}
 })
