1126a616f7867928f39eea2668300eca5e55ce0c
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-27 19:20:34 +0200

    remove comment

--- packages/sidebar/vite.config.ts
+++ packages/sidebar/vite.config.ts
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-	
-  // TODO - need to remove ASSETS from built JS files
   base: "./",
 	build: {
 		outDir: `${process.env.INIT_CWD}/dist/assets/sidebar`
