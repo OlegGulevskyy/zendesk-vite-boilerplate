1f202b3d904d869b1204472fdbf1e34d1ce632d4
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-28 17:34:53 +0200

    adjust build directory for svelte demo

--- packages/nav_bar/vite.config.ts
+++ packages/nav_bar/vite.config.ts
@@ -5,5 +5,10 @@ const { injectZafHtmlPlugin } = pluginPkg.default
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte(), injectZafHtmlPlugin()]
+  plugins: [svelte(), injectZafHtmlPlugin()],
+  base: "./",
+  build: {
+    outDir: `${process.env.INIT_CWD}/dist/assets/${process.env.ADDON_TYPE}`,
+    emptyOutDir: true
+  },
 })
