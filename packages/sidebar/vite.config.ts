c10fb14f6ddf45a312594bc2903aced717cbde61
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-28 16:47:08 +0200

    fix issues with zaf sdk types

--- packages/sidebar/vite.config.ts
+++ packages/sidebar/vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { injectZafHtmlPlugin } from '@app/zendesk/vite-plugin-inject-zaf-html'
+import { injectZafHtmlPlugin } from "@app/zendesk/vite-plugin-inject-zaf-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), injectZafHtmlPlugin()],
   base: "./",
-	build: {
-		outDir: `${process.env.INIT_CWD}/dist/assets/${process.env.ADDON_TYPE}`,
-		emptyOutDir: true,
-	}
-})
+  build: {
+    outDir: `${process.env.INIT_CWD}/dist/assets/${process.env.ADDON_TYPE}`,
+    emptyOutDir: true
+  },
+});
--- packages/zendesk/sdk/index.ts
+++ packages/zendesk/sdk/index.ts
@@ -1,7 +1,31 @@
 // The ZAFClient is injected in the index.html file when built / served.
 // See docs for help regarding the ZAFClient: https://developer.zendesk.com/apps/docs/developer-guide/getting_started
 
-let zafClient: ReturnType<typeof window.ZAFClient["init"]>;
+interface IMetadata {
+  appId: number;
+  installationId: number;
+  name: string;
+  plan: {
+    name: string;
+  };
+}
+
+interface IClient {
+  invoke: (cmd: string, arg: any) => void;
+  get: (getter: string) => any;
+  metadata: () => IMetadata;
+  request: <U>(data: Object) => Promise<U>;
+}
+
+declare global {
+	interface Window {
+		ZAFClient: {
+			init: () => IClient
+		}
+	}
+}
+
+let zafClient: IClient;
 if (typeof window.ZAFClient === "undefined") {
   // eslint-disable-line no-undef
   throw new Error("ZAFClient cannot run outside Zendesk");
