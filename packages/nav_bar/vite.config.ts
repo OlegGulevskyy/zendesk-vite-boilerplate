c835221565dd13d7db99811149dda97f0c302ee4
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2022-06-28 16:58:47 +0200

    import zaf html injection plugin

--- packages/nav_bar/vite.config.ts
+++ packages/nav_bar/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import * as pluginPkg from '@app/zendesk/vite-plugin-inject-zaf-html/index.js'
+const { injectZafHtmlPlugin } = pluginPkg.default
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte(), injectZafHtmlPlugin()]
 })
