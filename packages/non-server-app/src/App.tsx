c50418d816fa70b66239cf4d3ad8efd17a647f77
Oleg Gulevskyy <43781031+OlegGulevskyy@users.noreply.github.com>
2024-10-19 09:26:08 +0200

    prettify a tiny bit

--- packages/non-server-app/src/App.tsx
+++ packages/non-server-app/src/App.tsx
@@ -6,8 +6,8 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
+    <div className="flex flex-col gap-4 items-center justify-center mt-4">
+      <div className="flex flex-row gap-4">
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,18 +16,18 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className="flex flex-col">
+        <button
+          className="bg-indigo-300 p-2 rounded-md mx-auto"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
